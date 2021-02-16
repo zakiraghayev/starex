9122f1f12d207e80df4070366adb1f29ea6da774
Zakir Aghayev <developer.stuterm@gmail.com>
2021-02-16 10:21:53 +0400

    home page Test added

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class HomeTest(TestCase):
+
+    def test_get_home_page(self):
+        """ testing home page found. """
+        c = Client()
+        res = c.get('/')
+        self.assertEqual(res.status_code, 200)
