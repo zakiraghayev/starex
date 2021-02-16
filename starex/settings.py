052ffdeef5602ac54be049dd832143d04df04592
Zakir Aghayev <developer.stuterm@gmail.com>
2021-02-16 10:17:13 +0400

    home page added

--- starex/settings.py
+++ starex/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2rfbq&#=w5)%p%sa#_g$z18j*r)#65@furgxt4m(2x%6ox0&7&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'home',
 ]
 
 MIDDLEWARE = [
